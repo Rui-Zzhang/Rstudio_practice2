2870e732aa837c2a5d8045dd627fddaee7e5af23
Rui-Zzhang <zzhan186@uottawa.ca>
2021-02-05 15:10:14 +0800

    lmm practical

--- BIO8102_practical_github.R
+++ BIO8102_practical_github.R
@@ -5,15 +5,22 @@ unicorns <- read.csv("unicorns_aggression.csv")
 summary(unicorns)
 
 install.packages("lme4")
+install.packages("lmerTest")
 library("Matrix")
 library("lme4")
+library("nlme")
+library("lmerTest")
 
 #Fit a first mixed model with lmer that have only individual identity as a random effect and only a population mean.
 M1 <- lmer(aggression ~ 1 + (1|ID), data = unicorns)
+M1
 summary(M1)
 
 # This is a fairly rubbish model so now fit a better model by adding opponent size (opp_size) and block (block) as fixed effects. 
 M2 <- lmer(aggression ~ opp_size + block + (1|ID), data = unicorns)
 summary(M2)
+
+#Load lmerTest and refit the model using the exact same code. Look at the summary again.
+
 
 
