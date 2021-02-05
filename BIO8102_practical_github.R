af48922133d3db5d5bd6ad900776186011156c94
Rui-Zzhang <zzhan186@uottawa.ca>
2021-02-05 14:44:53 +0800

    LMM practical

--- BIO8102_practical_github.R
+++ BIO8102_practical_github.R
@@ -1,9 +1,19 @@
-# these two files are very different
+#Lecture3
+#LMM practical 
 
-library(palmerpenguins)
-data(package = 'palmerpenguins')
-head(penguins)
+unicorns <- read.csv("unicorns_aggression.csv")
+summary(unicorns)
 
-#I added something here
-1+1
-4 * 8^7 
+install.packages("lme4")
+library("Matrix")
+library("lme4")
+
+#Fit a first mixed model with lmer that have only individual identity as a random effect and only a population mean.
+M1 <- lmer(aggression ~ 1 + (1|ID), data = unicorns)
+summary(M1)
+
+# This is a fairly rubbish model so now fit a better model by adding opponent size (opp_size) and block (block) as fixed effects. 
+M2 <- lmer(aggression ~ opp_size + block + (1|ID), data = unicorns)
+summary(M2)
+
+
